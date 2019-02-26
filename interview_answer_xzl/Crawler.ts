2e8299dd2e5d355a781f2b4eedfcc1febde57524
superXLL <45613812+superXLL@users.noreply.github.com>
2019-02-26 21:41:39 +0800

    Update

--- interview_answer_xzl/Crawler.ts
+++ interview_answer_xzl/Crawler.ts
@@ -1,8 +1,10 @@
+//1. 从多个网页爬取内容，然后进行解析
+
 function loadPage(url) {
-    var http = require('http');
-    var pm = new Promise(function (resolve, reject) {
+    const  http = require('http');
+    const  pm = new Promise(function (resolve, reject) {
         http.get(url, function (res) {
-            var html = '';
+            let html = '';
             res.on('data', function (d) {
                 html += d.toString()
             });
@@ -16,20 +18,20 @@ function loadPage(url) {
     return pm;
 }
 
-var data_count = 0;
+let data_count = 0;
 async function crawl(){
-        for (var i=1;i<11;i++) {
-        await loadPage('http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/'+i+'.txt').then(function (d) {
-        var str = d;
-        reg = /data : ([\s\S]+); /gi;
-        var result;
-        if ((result = reg.exec(str)) != null) {
-            data_num = parseInt(result[1]);
-            data_count = data_count+data_num;
-            console.log(i+".txt的data值为"+data_num);
+        for (let i=1;i<11;i++) {
+            await loadPage('http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/'+i+'.txt').then(function (d) {
+            let str = d;
+            reg = /data : ([\s\S]+); /gi;
+            let result ;
+            if ((result = reg.exec(str)) != null) {
+                data_num = parseInt(result[1]);
+                data_count = data_count+data_num;
+                console.log(i+".txt的data值为"+data_num);
+                }
+            });
         }
-    });
-    }
         console.log("所有data值的和为"+data_count);
 }
 crawl();
